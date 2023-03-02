bd7f39ac7b7c8d4b0bc02ce266e98967f2456a0a
ysv <ysv@example.com>
2023-03-02 20:37:38 +0300

    new edit

--- num00.py
+++ num00.py
@@ -1,1 +1,15 @@
-#new file
+#new code
+
+f = open('/home/asd/Рабочий стол/Web/24_1.txt')
+s = f.readline()
+f.close
+
+l = lmax = 1
+
+for i in range(len(s)-1):
+    if s[i] == 'A' and s[i+1] == 'A':
+        l += 1
+        lmax = max(l, lmax)
+    else:
+        l = 1
+print(lmax)
